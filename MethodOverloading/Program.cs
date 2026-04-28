37ca4585f889013056cde99ab7cb25c4e52ccbf8
Raul Rios <rdrios04@gmail.com>
2026-04-28 10:23:18 -0700

    Overloaded a method

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,37 @@
 {
     public class Program
     {
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isCurrency)
+        {
+            var sum = numOne + numTwo;
+
+            if (isCurrency && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCurrency && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 1, false));
         }
     }
 }
